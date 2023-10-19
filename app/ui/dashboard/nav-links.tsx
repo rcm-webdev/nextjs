8a00d6d752d39ed4b83644d0878152f8c3ccef25
rcm-webdev <rcm.webdev.pro@gmail.com>
2023-10-18 19:06:44 -0700

    add clsx to show active links on dashboard

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -6,8 +6,14 @@ import {
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
 
+//use Link to help render links before user even clicks on link
 import Link from 'next/link';
+
+//usePathname to help show active links
 import { usePathname } from 'next/navigation';
+
+//use clsx to help users identify which page they are on
+import clsx from 'clsx';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -31,7 +37,11 @@ export default function NavLinks() {
           <Link
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+            className={clsx(
+              "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3", {
+                'bg-sky-100 text-blue-600': pathname === link.href,
+              },
+            )}
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
